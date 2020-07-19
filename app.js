5b95cf6e8399bbb8246bc58f4e5d258937a5b944
Naomi, Cassandra <cat.monden@gmail.com>
2020-07-19 17:22:17 -0400

    socket io installation/prelim setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// express setup
+// setup
 
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var express = require('express');
 var app = express();
 
@@ -12,20 +14,49 @@ app.listen(8080, () => {
 
 // state variables
 
-var students = 0;
-var lecturers = 0;
+const lecturers = io.of("/lecture");
+const students = io.of("/student");
 
-app.get('/student/', function(req, res) {
+app.get('/student', function(req, res) {
 	res.sendFile(__dirname + '/views/student.html');
-	students++;
-	console.log("A student has connected. Student count: " + students);
+	console.log("A student has been sent student page.");
 });
 
-app.get('/lecture/', function(req, res) {
-	res.sendFile(__dirname + '/views/lecture.html');
-	lecturers++;
-	console.log("A lecturer has connected. Lecturer count: " + lecturers);
+students.use((socket, next) => {
+	next();
 });
+
+students.on('connection', socket => {
+	console.log("Student " + socket.id + " has connected.");
+	socket.on('disconnect', () => {
+		console.log("Student " + socket.id + " disconnected.");
+	});
+});
+
+app.get('/lecture', function(req, res) {
+	res.sendFile(__dirname + '/views/lecture.html');
+	console.log("A lecturer has been sent student page.");
+});
+
+lecturers.use((socket, next) => {
+	next();
+});
+
+lecturers.on('connection', socket => {
+	console.log("Lecturer " + socket.id + " has connected.");
+	socket.on('disconnect', () => {
+		console.log("Lecturer " + socket.id + " disconnected.");
+	});
+});
+
+/*io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});*/
+
+
 
 // student request handling
 
@@ -104,4 +135,18 @@ app.post('/lecture_exit', (req, res) => {
 
 app.get('/lecture_get_all', (req, res) => {
 	res.send(JSON.stringify(feedbacks));
-})
+});
+
+app.get('/lecture_get_feedback/', (req, res) => {
+	var validFeedbacks = new Array();
+
+	var currentTime = new Date();
+	feedbacks.forEach(feedback => {
+		if (feedback.time >= currentTime - 60000) {
+			validFeedbacks.push(feedback);
+		}
+	});
+	console.log("feedback response:");
+	console.log(JSON.stringify(validFeedbacks));
+	res.send(JSON.stringify(validFeedbacks));
+});
--- public/scripts/client-lecture.js
+++ public/scripts/client-lecture.js
@@ -1,10 +1,57 @@
 console.log("lecture client running!");
 
 // main feedback loop
+var LOOP_INTERVAL = 15000; // 15 seconds
+var FEEDBACK_DURATION = 60000; // 1 minute
 
 setInterval(() => {
-console.log("15 seconds!");
+	console.log("15 seconds");
+	fetch('/lecture_get_feedback', {
+			method: 'GET',
+			headers: {'content-type': 'application/json' }
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			parseFeedback(data);
+		})
+		.catch(error => console.log(error));
 }, 15000);
+
+var receivedFeedbacks = new Array();
+function parseFeedback(feedbacks) {
+	console.log(feedbacks);
+
+	var relevantFeedbacks = new Array();
+
+	var results = {
+		positive: 0,
+		negative: 0,
+		repeat: 0,
+		question: 0
+	}
+	feedbacks.forEach(feedback => {
+		if (!receivedFeedbacks.includes(feedback)) {
+			relevantFeedbacks.push(feedback);
+			receivedFeedbacks.push(feedback);
+
+			results[feedback.type]++;
+		}
+	});
+
+	console.log(results);
+}
+
+/*
+function hasReceived(newFeedback) {
+	received.forEach(feedback => {
+		if (newFeedback.type == feedback.type &&
+			newFeedback.time) {
+			return false;
+		}
+		else if (newFeedback.type == feedback)
+	});
+}*/
 
 // exit detection
 
